116e883750ae183145956ce92f0adb414c79b86f
Denis <marikoryagina.den@yandex.ru>
2024-02-17 14:19:36 +0300

    Задание готово

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var buttonScore: UIButton!
+    
+    var count = 0
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        count = count + 1
+        labelScore.text = "\(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        }
         // Do any additional setup after loading the view.
     }
 
 
-}
 
